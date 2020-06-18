d21727aa6de7bbbfb9f9b1be953de5fe19d83042
Nathan Chung <chung.nathanh@gmail.com>
2020-06-18 01:10:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(`${name}`);
+  katzDeliLine.push(name);
   return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    var nowServing = katzDeliLine[0];
-    katzDeliLine.shift()
-  return (`Currently serving ${nowServing}.`)
+    return(`Currently serving ${katzDeliLine.shift()}.`)
   } else {
-    return `There is nobody waiting to be served!`
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var line = [];
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    line.push(" " + [i + 1] + "." + " " + katzDeliLine[i])
+  }
+  
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    return "The line is currently:" + line;
   }
   }
   
-
-  function currentLine(people){
-  var line = [];
-    for (let i = 0; i < people.length; i++) {
-    line.push(" " + [i + 1] + "." + " " + people[i])
-  }
-  if (people.length === 0) {
-  return "The line is currently empty."
-    
-  } else {
-    return ("The line is currently:" + line)
-  }
-  }
+  
+  
